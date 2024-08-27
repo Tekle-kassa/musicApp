eaa49b9d3cea1ed9567daa308d809c4a5d9cb033
Teklee <kassateklee@gmail.com>
2024-08-28 00:36:32 +0300

    [UPD] modify the app.js to import the routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,12 @@ import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectdb from "./config/db";
+import songRoutes from "./routes/songRoutes";
 import { errorHandler } from "./middleware/errorHandler";
 const app: Application = express();
 
 app.use(express.json());
+app.use("/api", songRoutes);
 connectdb();
 app.get("/", (req: Request, res: Response) => {
   res.send("hello");
