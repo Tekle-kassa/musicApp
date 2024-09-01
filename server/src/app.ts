d05a4866011e9ecdc7bdabab7d5a068696aa2ce5
Teklee <kassateklee@gmail.com>
2024-09-01 16:23:12 +0300

    [ADD] handle cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,13 @@ import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectdb from "./config/db";
+import cors from "./config/cors";
 import songRoutes from "./routes/songRoutes";
 import { errorHandler } from "./middleware/errorHandler";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors);
 app.use("/api", songRoutes);
 connectdb();
 app.get("/", (req: Request, res: Response) => {
